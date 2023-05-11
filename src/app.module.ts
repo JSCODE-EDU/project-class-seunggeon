9c39572407b44da558c36d4872b81f5a7b058ed8
seunggeon <tmdrjs9555@naver.com>
2023-05-11 09:17:50 +0900

    refactor : modify gitignore & package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,14 +14,10 @@ import { ConfigModule } from "@nestjs/config";
     TypeOrmModule.forRoot({
       type: "mysql",
       host: process.env.DB_HOST,
-      // host: 'localhost',
       port: 3306,
       username: process.env.DB_USERNAME,
-      // username: 'root',
       password: process.env.DB_PASSWORD,
-      // password: '970412',
       database: process.env.DB_DATABASE,
-      // database: 'JSCODE_DB',
       entities: [
         Posts
       ],
